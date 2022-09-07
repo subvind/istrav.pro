ba75e8b47b8d1e57c0aacd455de32904b937c947
istrav <travis.burandt@gmail.com>
2022-09-07 17:00:21 -0500

    header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -22,12 +22,13 @@
     <div class="nav-wrapper">
       <ul class="left">
         <a href="#!" on:click={() => toggleSidebar()} class="black-text left"><i class="material-icons left">menu</i></a>
+        <li><a href="/tenant" class="black-text"><i class="material-icons left">business</i> demo</a></li>
+        <li><a href="/accounts" class="black-text"><i class="material-icons left">assignment_ind</i> annonymous</a></li>
+        <li><a href="#" class="black-text"><i class="material-icons left">notifications</i>(2)</a></li>
       </ul>
-      <a href="#" class="btn transparent black-text"><i class="material-icons left" style="line-height: 34px; margin-right: 5px;">assignment_ind</i> annonymous</a>
       <ul class="right">
-        <li><a href="#" class="black-text right"><i class="material-icons left">notifications</i>(2)</a></li>
-        <li><a href="#" class="black-text right"><i class="material-icons left">format_list_numbered</i>Work Orders (2)</a></li>
-        <li><a href="#" class="black-text right"><i class="material-icons left">local_shipping</i>Transports (0)</a></li>
+        <li><a href="#" class="black-text right"><i class="material-icons left">local_offer</i>Tasks (2)</a></li>
+        <li><a href="/accounts" class="btn transparent black-text"><i class="material-icons left" style="line-height: 34px; margin-right: 5px;">grid_on</i> fleet1</a></li>
         <!-- <li><a href="#" class="black-text"><i class="material-icons left">timer</i>10s</a></li>
         <li><a href="#" class="black-text"><i class="material-icons left">format_list_numbered</i>(16)</a></li>
         <li><a href="#" class="black-text"><i class="material-icons left">map</i>8 miles</a></li>
