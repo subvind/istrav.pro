8b352fe8c6534aa60e899c89f21414c09ed2a305
istrav <travis.burandt@gmail.com>
2022-09-10 15:59:19 -0500

    map

---
+++ src/components/Map.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const leaflet = await import('leaflet');
+
+    const map = leaflet.map('map', {
+      scrollWheelZoom: false
+    }).setView([30.2672, -97.7431], 13);
+
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    leaflet.marker([30.2672, -97.7431]).addTo(map)
+      .bindPopup('Located in Austin, TX')
+      .openPopup();
+  });
+</script>
+
+<div id="map"></div>
+
+<style>
+  @import 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css';
+  #map {
+    height: calc(100vh);
+    width: 100%;
+    -webkit-box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
+    box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%);
+
+  }
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
 
   import { backend, sidebarActive, sidebarMode } from '../../stores';
+  import Map from '../../components/Map.svelte'
 
   let api
   backend.subscribe(value => {
@@ -11,4 +12,18 @@
   sidebarActive.set('')
 </script>
 
-hello dashboard
+<Map />
+<div class="container" style="margin-top: -10px;">
+  <div class="card">
+    <div class="card-content">
+      <div style="text-align: center; margin-top: -3.3em;">
+        <a class="btn-large waves-effect waves-light blue lighten-2" style="z-index: 10000000;"><i class="material-icons">dashboard</i></a>
+      </div>
+      <br />
+      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+    </div>
+  </div>
+</div>
+<br />
+<br />
+<br />
