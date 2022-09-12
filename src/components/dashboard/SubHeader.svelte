cb5ac744e84e358a5246e7e690cd8f9f396a0c67
istrav <travis.burandt@gmail.com>
2022-09-11 20:21:58 -0500

    FleetOptimizer 2

---
+++ src/components/dashboard/SubHeader.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {});
+
+
+    var elems2 = document.querySelectorAll('.tabs');
+    var instances2 = M.Tabs.init(elems2, {});
+  })
+</script>
+<nav class="nav-extended">
+  <div class="nav-wrapper black">
+    <a href="/fleets/vehicles/" class="brand-logo" style="margin-left: 0.5em;"><i class="material-icons">directions_bus</i>test</a>
+    <ul class="right hide-on-med-and-down">
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown3">By Task<i class="material-icons right">arrow_drop_down</i></a></li>
+      <ul id="dropdown3" class="dropdown-content">
+        <li><a href="#!">one</a></li>
+        <li><a href="#!">two</a></li>
+        <li class="divider"></li>
+        <li><a href="#!">three</a></li>
+      </ul>
+    </ul>
+  </div>
+</nav>
+<div style="background: #000; overflow: hidden; padding: 0.5em;">
+  <a href="/fleets/tasks" class="left white-text" style="padding: 0.5em;"><i class="material-icons left">local_offer</i>Something Todo</a>
+  <a href="/fleets/tasks" class="btn white-text right disabled"><i class="material-icons left">local_shipping</i>Transport</a>
+</div>
+<div class="row">
+  <div class="col s12">
+    <ul class="tabs black">
+      <li class="tab col s3"><a href="#test1">Pickup</a></li>
+      <li class="tab col s3"><a class="active" href="#test2">Delivery</a></li>
+    </ul>
+  </div>
+  <div id="test1" class="col s12">Test 1</div>
+  <div id="test2" class="col s12">Test 2</div>
+</div>
+      
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,6 +4,7 @@
   import { backend, sidebarActive, sidebarMode } from '../../stores';
   import Map from '../../components/Map.svelte'
   import Header from '../../components/dashboard/Header.svelte'
+  import SubHeader from '../../components/dashboard/SubHeader.svelte'
 
   let api
   backend.subscribe(value => {
@@ -17,10 +18,11 @@
 <Map />
 <div class="container" style="margin-top: -10px;">
   <div class="card">
+    <div style="text-align: center; margin: -2.5em 0 -1.6em 0;">
+      <a class="btn-large waves-effect waves-light blue lighten-2" style="z-index: 10000000;"><i class="material-icons">dashboard</i></a>
+    </div>
+    <SubHeader />
     <div class="card-content">
-      <div style="text-align: center; margin-top: -3.3em;">
-        <a class="btn-large waves-effect waves-light blue lighten-2" style="z-index: 10000000;"><i class="material-icons">dashboard</i></a>
-      </div>
       <br />
       <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
     </div>
